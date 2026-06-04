5a7819b703f7c9ac0486edfae97de79d7004b09d
mightyquinn408 <mityquinn@me.com>
2026-06-04 17:01:00 -0600

    Initial Commit

---
+++ WashRackPrototype/WashRackPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashRackPrototypeApp.swift
+//  WashRackPrototype
+//
+//  Created by Michael Quinn on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct WashRackPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WashRackPrototypeTests/WashRackPrototypeTests.swift
@@ -0,0 +1,19 @@
+//
+//  WashRackPrototypeTests.swift
+//  WashRackPrototypeTests
+//
+//  Created by Michael Quinn on 6/4/26.
+//
+
+import Testing
+@testable import WashRackPrototype
+
+struct WashRackPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
